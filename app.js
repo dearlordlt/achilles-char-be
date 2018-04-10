14599f326578edc67e7d6fc4a9d73e4850395692
ssliteris <ssliteris@exadel.com>
2018-04-10 21:32:52 +0300

    access allow

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ const authMiddleware = auth.connect(basic);
  * MIDDLEWARE
  */
 app.use(morgan('tiny'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials");
+    next();
+});
 
 /**
  * CONFIG
